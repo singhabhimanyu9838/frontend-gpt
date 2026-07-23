f3ab108acaf6fa0820a66cbddd77c9906bb3036e
Abhimanyu Singh <singhabhimanyu9838@gmail.com>
2026-07-23 22:55:50 +0530

    connect

--- src/ChatWindow.jsx
+++ src/ChatWindow.jsx
@@ -26,7 +26,7 @@ function ChatWindow() {
         };
 
         try {
-            const response = await fetch("http://localhost:8080/api/chat", options);
+            const response = await fetch("http://ec2-13-60-197-171.eu-north-1.compute.amazonaws.com:8080/api/chat", options);
             const res = await response.json();
             console.log(res);
 
